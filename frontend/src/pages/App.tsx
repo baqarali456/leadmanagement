f162bd7c6cfd83bd2af4cffb3852231424b6f252
baqar <sayedalintl@gmail.com>
2025-09-03 19:00:11 +0530

    add frontend directory

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Lead Enquiry Form</h1>
+      {/* Lead form component here */}
+      {/* Lead list component here */}
+    </div>
+  );
+};
+
+export default App;
